0da457e7d459fdef79cc61b09fe42e338f50b1aa
Mukund Tiwari <mukundtiwari251103@gmail.com>
2024-03-02 10:16:00 +0530

    first episode

---
+++ App.js
@@ -0,0 +1,42 @@
+let content = "Hello world Using React!";
+let element = React.createElement("h1", { className: "this object is used to give attributes to the elements" }, content);
+let root = ReactDOM.createRoot(document.querySelector('.root'));
+root.render(element);
+
+let parent = React.createElement(
+    'div',
+    { id: 'parent' },
+    [React.createElement(
+        'div',
+        { id: 'child' },
+        [
+            React.createElement(
+                'h1',
+                {},
+                "I am h1 tag!"
+            ),
+            React.createElement(
+                'h2',
+                {},
+                "I am h1 tag!"
+            )]
+    ),
+    React.createElement(
+        'div',
+        { id: 'child2' },
+        [
+            React.createElement(
+                'h1',
+                {},
+                "I am h1 tag!"
+            ),
+            React.createElement(
+                'h2',
+                {},
+                "I am h1 tag!"
+            )]
+    )
+    ]
+)
+
+root.render(parent)
---
+++ Readme.md
@@ -0,0 +1,1 @@
+#This repository is Created during learning React by Namaste React.
